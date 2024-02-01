d1a5ffcd3ab2f2bd5587925ffc796e9ff3234b6a
Karel Smutný <karel.smutny@outlook.com>
2024-02-01 10:14:13 +0100

    commit 4

--- gilded_rose.py
+++ gilded_rose.py
@@ -7,10 +7,7 @@ class GildedRose(object):
         for item in self.items:
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
-            if not (item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"):
-                if item.quality > 0:
-                    item.quality -= 1
-            else:
+            if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality < 50:
                     item.quality += 1
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
@@ -20,6 +17,10 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality += 1
+            else:
+                if item.quality > 0:
+                    item.quality -= 1
+
 
             item.sell_in -= 1
 
